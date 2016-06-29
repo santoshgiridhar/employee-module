f8d3dd670ba5c7a27b08bcd766e37be2f3491b82
Santosh G Ksheersagar <Santosh G Ksheersagar>
2016-06-29 23:26:54 +0530

    Included add employee module

---
+++ components/page-employee/add-employee/add-employee.js
@@ -0,0 +1,16 @@
+import can from 'can';
+import Component from 'can/component/';
+import './style.less!';
+import VM from './view-model';
+import template from './template.stache!';
+
+can.Component.extend({
+    tag: 'add-employee',
+    viewModel: VM,
+    template: template,
+    events: {
+        'inserted': function() {
+            console.log('loaded add employee');
+        }
+    }
+});
